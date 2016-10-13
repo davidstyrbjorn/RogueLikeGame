9638cbd6def3544a487b72fcafa0f6411fd3615f
DavidStyrbjorn <davidstyrbjorn@gmail.com>
2016-10-13 16:23:42 +0200

    Base values changed

--- Assets/Scripts/BaseValues/BaseValues.cs
+++ Assets/Scripts/BaseValues/BaseValues.cs
@@ -10,8 +10,8 @@ public class BaseValues{
     // Enemy base values
     public static float FloorAttackModifier = 1.25f;
     public static float FloorHealthModifier = 1.25f;
-    public static float EnemyBaseHP = 50;
-    public static float EnemyBaseAttack = 10;
+    public static float EnemyBaseHP = 100;
+    public static float EnemyBaseAttack = 5;
 
     // Weapon stats
     public static float WeaponCriticalMultiplier = 1.6f;
